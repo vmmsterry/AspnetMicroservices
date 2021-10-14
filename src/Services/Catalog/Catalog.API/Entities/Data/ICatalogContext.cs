bf1c59e27fcf382a6d5346b752cb2c9472d7bf1d
VMMSTERRY <vmms.terry@gmail.com>
2021-10-14 18:41:38 -0500

    Catalog.API-MongoDB

---
+++ src/Services/Catalog/Catalog.API/Entities/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Entities.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
